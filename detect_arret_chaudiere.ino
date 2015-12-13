4890c1248c8493403e2765a441f73d731543c2c1
cheperboy <matjouve@gmail.com>
2015-12-13 16:25:10 +0100

    en cours : idle mode

--- detect_arret_chaudiere.ino
+++ detect_arret_chaudiere.ino
@@ -7,11 +7,21 @@
 #include "lcd.h"
 
 #define ADDRESS 1
+
+enum running_mode {
+  up,
+  idle
+};
+running_mode GO_IDLE = idle;
+running_mode GO_UP = up;
+
 int value = 0;
 
 int consigne = 0;
 int consigne_init = 0;
-
+int button = -1;
+int sleep = millis();
+int mode = 1;
 Deuligne lcd;
 void setup()
 {
@@ -19,22 +29,18 @@ void setup()
 	lcd.init();
 	lcd.setCursor(0, 0);
 	lcd.print("init");  
-
-	delay(3000);
+	delay(700);
 	
 //	EEPROM.write(ADDRESS, 15);
-
 	consigne = EEPROM.read(ADDRESS);
-
-/*
-	char buffer[5];
-	sprintf(buffer, "%d", value);
-	print_custom (buffer);
-*/
-
+	char buffer[16];
+	sprintf(buffer, "consigne = %d", consigne);
+	lcd.setCursor(0, 1);
+	lcd.print(buffer);  
+	delay(2000);
+	lcd.clear();
 //	while (1);
   }
-
 
 void save_consigne (){
 	int mem = EEPROM.read(ADDRESS);
@@ -66,34 +72,68 @@ void print_temp (float temp){
 //	Serial.println(temp);
 }
 
-void print_and_update_consigne(){
-	
+void print_and_update_consigne(int value){
+	consigne = consigne + value;
+	print_consigne(consigne);
+	delay(300);	
+}
+
+void manage_button() {
+	unsigned long time;
+	time = millis();
+	print_consigne(consigne);
+	delay(1000);
+	while(millis() < time + 4000) {		
+		button=lcd.get_key();
+		delay(100); // for debounce
+		if(button == LEFT || button == RIGHT || button == UP || button == DOWN){
+			value = 0;
+			if(button == LEFT) {value = -1; }
+			if(button == RIGHT) {value = 1;}
+			if(button == DOWN) {value = -5;}
+			if(button == UP) {value = 5;}
+		time = millis();
+		print_and_update_consigne(value);
+		}
+	}
+	save_consigne ();
+	lcd.clear();
+	button = -1;
+	sleep = millis();
+}
+
+void switch_mode(int value){
+	mode = value;
+	if(mode == up) {lcd.display();}
+	if(mode == idle) {lcd.noDisplay();}
+}
+
+void check_consigne(float temp, int consigne){
+	if(temp < consigne){
+		lcd.clear();
+		lcd.setCursor(0, 0);
+		lcd.print("ALERT");  
+		lcd.setCursor(0, 0);
+		lcd.print("ALERT !!!");
+		delay(1000);
+	}
 }
 
 void loop() {
-	unsigned long time;
-	delay(500);
-	int button = -1;
 	
 	float temp = temp_as_c(); // get temp value
 	print_temp (temp);
-	
+	delay(1000);
+	check_consigne(temp, consigne);
+
 	button=lcd.get_key();
 	delay(100); // for debounce
-	if(button > -1) {
-		time = millis();
-		print_consigne(consigne);
-		delay(1000);
-		while(millis() < time + 3000) {		
-			button=lcd.get_key();
-			delay(100); // for debounce
-			if(button == LEFT) {--consigne;time = millis();print_consigne(consigne);delay(300);}
-			if(button == RIGHT) {consigne++;time = millis();print_consigne(consigne);delay(300);}
-			if(button == DOWN) {consigne=consigne-5;time = millis();print_consigne(consigne);delay(300);}
-			if(button == UP) {consigne=consigne-5;time = millis();print_consigne(consigne);delay(300);}
-		}
-		save_consigne ();
-		lcd.clear();
+	if(button > -1 && mode == up) {
+		manage_button();
 	}
-	
+	if(button > -1 && mode == idle) {
+		sleep = millis();
+		switch_mode(1);
+	}
+	if(millis() > sleep + 6000){switch_mode(0);}	
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
-
+//#include "Arduino.h"
 int Arrondi(float nombre_float) { 
 	int nombre = int(nombre_float);
 	if(nombre_float - nombre  >= 0.5) nombre++; 
 	return nombre; 
 }
+
+
